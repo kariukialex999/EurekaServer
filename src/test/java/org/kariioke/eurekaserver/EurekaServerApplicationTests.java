ef5a54e4e896bd20e3da223fc4e368121ab002c4
alex kariuki <kariukialex999@gmail.com>
2026-04-12 14:39:19 +0300

    initial commit

---
+++ src/test/java/org/kariioke/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kariioke.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
